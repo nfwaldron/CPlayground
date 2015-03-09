a3a780605ef37052f95efb6c31a1c26eff0941f5
Nathan Waldron <waldron.nathan@gmail.com>
2015-03-09 10:20:34 -0400

    Update ExpoGenerator.cpp

--- RandomNumberGenerator/RandomNumberGenerator/ExpoGenerator.cpp
+++ RandomNumberGenerator/RandomNumberGenerator/ExpoGenerator.cpp
@@ -1,78 +1,25 @@
-//
-//  ExpoGenerator.cpp
-//  RandomNumberGenerator
-//
-//  Created by Admin on 3/2/15.
-//  Copyright (c) 2015 Admin. All rights reserved.
-//
-
 #include "ExpoGenerator.h"
-#include <cmath>
-#include <iostream>
-using namespace std;
 
-ExpoGenerator::ExpoGenerator()
-{
-    cout<<"ExpoGenerator constructor called"<<endl;
+ExpoGenerator::ExpoGenerator(){
+    lambda = 0.0;
+}
+ExpoGenerator::ExpoGenerator(double lambda){
+    this->lambda = lambda;
 }
 
-
-void ExpoGenerator::setParameters( int bits, int lambda)
-{
-    // set the number of bits that the .random method will use to generate a random number
-    if (bits < 1.0)
-    {
-        cerr << "You have entered an invalid value please enter a value greater than '1' "<<endl;
-    }
-    else
-    {
-    myBits = bits;
-    }
-    
-    // set the value of lambda
-    myLambda = lambda;
-    
-    // set the range
-    myRange = pow(2.0, myBits);
-    
-    cout << "bits = " << myBits << " lambda = " << myLambda << " Range = "<< myRange << endl;
-   
-    //get a random number
-    
-    unsigned long int result = random(bits);
-
-    cout << "My random number = " << result << endl;
-//
-//    //set the value of t
-//    myT = myRandomNumber/(myBits-1);
-//    
-//    cout << "My t  = " << myT <<endl;
-
+void ExpoGenerator::setLambda(double lambda){
+    this->lambda = lambda;
 }
-
-
-float ExpoGenerator::exponentialFunction()
-{
-    float result;
-    
-    
-    
-    
-    result = myLambda * pow(myE, (-(myLambda) * myT));
-    return result;
+double ExpoGenerator::getLambda(){
+    return lambda;
 }
-
-void ExpoGenerator::exponentialNumberGenerator()
-{
-    float* arrayPointer = new float[myRange];
-    float result;
-    for (int i = 0; i < 10000; i++)
-    {
-        result = exponentialFunction();
-        cout << result;
-        arrayPointer[i] = result;
-        arrayPointer[i]++;
-    }
-    
+double ExpoGenerator::ExpRandom(int bits){
+    double base = pow(2,bits) - 1;
+    unsigned long int temp = random(bits);
+    double lam = getLambda();
+    double temp2 = (double)temp / base;
+    return (lam*pow(e, -lam*temp2));
 }
-
+double ExpoGenerator::ExpectedMean(){
+    return 1 / getLambda();
+}
