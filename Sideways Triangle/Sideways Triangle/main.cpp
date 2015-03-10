7f69b88cae1cf13145175194a041b7acc7074fbd
Nathan Waldron <waldron.nathan@gmail.com>
2015-03-10 19:46:49 -0400

    Sideways Triangle
    Update

--- Sideways Triangle/Sideways Triangle/main.cpp
+++ Sideways Triangle/Sideways Triangle/main.cpp
@@ -21,11 +21,13 @@ int main(int argc, const char * argv[])
 {
     // Initialize the loop variable row. "row" = 1. While row is less than or equal to 5
     // execute the statement within the for loop and then increment the value of row
-    for (int row = 1; row <= 5; row++)
+    for (int row = 1; row <= 7; row++)
     {
         // While hash is less than or equal to row, exexute the statement inside the for loop
-        // and 
-        for (int hash = 1; hash <= row; hash++)
+        // which is to output a star. The condition needed to achieve the desired effects is
+        // While has is less than or equal to 4 minus the absolute value of 4 minus a row. We need the
+        // number to go up, and then go down.
+        for (int hash = 1; hash <= (4 - abs(4-row)); hash++)
         {
             cout << "#";
         }
